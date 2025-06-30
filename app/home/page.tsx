7ac90367e42b6acad13e70b9ffac3e475ac74aaa
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2025-06-30 16:57:23 +0000

    added data to test UI

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,6 +10,7 @@ import HeaderSection from "../components/HeaderSection";
 import Search from "../components/Search";
 import Filters from "../components/Filters";
 import Pagination from "../components/Pagination";
+import { speciesCards } from "../testData";
 
 export default function RentalDashboard() {
 	return (
@@ -53,43 +54,15 @@ export default function RentalDashboard() {
 				>
 					<Filters />
 					<Stack spacing={2} sx={{ overflow: "auto" }}>
-						<RentalCard
-							title="Capibarra"
-							category="Mammals"
-							rareFind
-							image="https://images.unsplash.com/photo-1568605114967-8130f3a36994?auto=format&fit=crop&w=400"
-						/>
-						<RentalCard
-							title="Designer NY style loft"
-							category="Entire loft in central business district"
-							liked
-							image="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?auto=format&fit=crop&w=400"
-						/>
-						<RentalCard
-							title="5 minute walk from University of Melbourne"
-							category="Entire rental unit in Carlton"
-							image="https://images.unsplash.com/photo-1537726235470-8504e3beef77?auto=format&fit=crop&w=400"
-						/>
-						<RentalCard
-							title="Magnificent apartment next to public transport"
-							category="Entire apartment rental in Collingwood"
-							image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?auto=format&fit=crop&w=400"
-						/>
-						<RentalCard
-							title="Next to shoppng mall and public transport"
-							category="Entire apartment rental in Collingwood"
-							image="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?auto=format&fit=crop&w=400"
-						/>
-						<RentalCard
-							title="Endless ocean view"
-							category="A private room in a shared apartment in Docklands"
-							image="https://images.unsplash.com/photo-1564013799919-ab600027ffc6?auto=format&fit=crop&w=400"
-						/>
-						<RentalCard
-							title="A Stylish Apt, 5 min walk to Queen Victoria Market"
-							category="one bedroom apartment in Collingwood"
-							image="https://images.unsplash.com/photo-1481437156560-3205f6a55735?auto=format&fit=crop&w=400"
-						/>
+						{speciesCards.map((species, index) => (
+							<RentalCard
+								key={index}
+								title={species.title}
+								category={species.category}
+								image={species.image}
+								liked={species.liked}
+							/>
+						))}
 					</Stack>
 				</Stack>
 				<Pagination />
---
+++ app/testData.tsx
@@ -0,0 +1,38 @@
+export const speciesCards = [
+	{
+		title: "Capybara",
+		category: "Mammal",
+		image: "https://images.unsplash.com/photo-1557431177-36141475c676?w=900&auto=format&fit=crop&q=60",
+	},
+	{
+		title: "Bald Eagle",
+		category: "Bird",
+		image: "https://images.unsplash.com/photo-1557401620-67270b61ea82?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFsZCUyMGVhZ2xlfGVufDB8fDB8fHww",
+		liked: true,
+	},
+	{
+		title: "Green Sea Turtle",
+		category: "Marine Reptile",
+		image: "https://images.unsplash.com/photo-1496196614460-48988a57fccf?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JlZW4lMjBzZWElMjB0dXJ0bGV8ZW58MHx8MHx8fDA%3D",
+	},
+	{
+		title: "Ring-tailed Lemur",
+		category: "Primate",
+		image: "https://images.unsplash.com/photo-1704175970578-e0e1d83536f0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmluZyUyMHRhaWxlZCUyMGxlbXVyfGVufDB8fDB8fHww",
+	},
+	{
+		title: "African Elephant",
+		category: "Mammal",
+		image: "https://plus.unsplash.com/premium_photo-1661828767985-20b3cf9d2c0b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YWZyaWNhbiUyMGVsZXBoYW50fGVufDB8fDB8fHww",
+	},
+	{
+		title: "Snow Leopard",
+		category: "Mammal – Endangered",
+		image: "https://plus.unsplash.com/premium_photo-1664303899642-88b294481198?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c25vdyUyMGxlb3BhcmR8ZW58MHx8MHx8fDA%3D",
+	},
+	{
+		title: "Poison Dart Frog",
+		category: "Amphibian",
+		image: "https://images.unsplash.com/photo-1650201777038-31d58a99afd6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9pc29uJTIwZGFydCUyMGZyb2d8ZW58MHx8MHx8fDA%3D",
+	},
+];
